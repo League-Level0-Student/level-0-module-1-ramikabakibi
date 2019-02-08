d4ecf63f5449ae0d9e14cc41b263a8d348a9d707
League <league@WTS8.attlocal.net>
2019-02-07 16:59:46 -0800

    Completed Unbirthday, Remarkable(IP)

---
+++ src/Remarkable2.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = JOptionPane.showInputDialog("Whats your nameeee?");
+		if (str.equals("Daniel")) {
+			JOptionPane.showMessageDialog(null, "Your'e a god at programming");
+		} else if (str.equals("Joaquin")) {
+			;
+		JOptionPane.showMessageDialog(null, "Can u play Brawl Stars today?");
+
+	}else if(str.equals("Rami")) {
+JOptionPane.showMessageDialog(null, "You have a fat cat");
+}
+}
+}
---
+++ src/Unbirthday.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = JOptionPane.showInputDialog("What is your birthday?");
+		if (str.equals("2/7/19")) {
+			JOptionPane.showMessageDialog(null, "Happpppppy Birrrrrttthhhdddaayy");
+
+		} else {
+JOptionPane.showMessageDialog(null, "YOUR'E TRASH KID");
+		}
+	}
+}
